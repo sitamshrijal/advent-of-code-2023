1ffdfee36cdb4b803e58a222f5be6b35ae35f358
Sitamsh Rijal <sitamshrijal@gmail.com>
2023-12-08 13:49:15 +0545

    Add solution for Day 1 Part 2

--- src/Day1.kt
+++ src/Day1.kt
@@ -4,7 +4,36 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val map = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9
+        )
+
+        val mapped = input.map { line ->
+            buildString {
+                line.forEachIndexed { index, c ->
+                    if (c.isDigit()) {
+                        append(c)
+                    } else {
+                        val substring = line.substring(index)
+
+                        map.forEach { (key, value) ->
+                            if (substring.startsWith(key)) {
+                                append(value)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return mapped.sumOf { it.calibrationValue() }
     }
 
     val input = readInput("input1")
