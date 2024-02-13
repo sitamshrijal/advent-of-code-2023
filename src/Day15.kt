c299348cb81920879c88f6275d7171479daa3703
Sitamsh Rijal <sitamshrijal@gmail.com>
2024-02-13 17:56:23 +0545

    Add solution for Day 15 Part 2

--- src/Day15.kt
+++ src/Day15.kt
@@ -8,7 +8,29 @@ fun main() {
     }
 
     fun part2(input: String): Int {
-        return input.length
+        val steps = input.split(",")
+
+        val boxes = List<MutableMap<String, Int>>(256) { mutableMapOf() }
+
+        for (step in steps) {
+            if (step.endsWith("-")) {
+                val label = step.removeSuffix("-")
+                val hash = label.hash()
+                boxes[hash] -= label
+            } else {
+                val label = step.substringBefore("=")
+                val hash = label.hash()
+                val focalLength = step.substringAfter("=").toInt()
+                boxes[hash][label] = focalLength
+            }
+        }
+        var sum = 0
+        boxes.forEachIndexed { i, contents ->
+            contents.onEachIndexed { j, lens ->
+                sum += (i + 1) * (j + 1) * lens.value
+            }
+        }
+        return sum
     }
 
     val input = Path("src/input15.txt").readText()
